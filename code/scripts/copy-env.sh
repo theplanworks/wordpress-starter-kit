7dcb32b7c0d062d6372edccd778a9ade75f618fc
Matt Fox <matt.fox@theplanworks.com>
2017-01-19 12:11:32 -0500

    updating scripts

--- code/scripts/copy-env.sh
+++ code/scripts/copy-env.sh
@@ -2,5 +2,5 @@
 
 # Copy the sample .env file if it does not exist
 
-cp -n ../../.env.sample ../../.env
+cp -n .env.sample .env
 echo ".env file copied. Please update with site-specific information."
--- code/scripts/migrate.sh
+++ code/scripts/migrate.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
 # Migrate DB from one domain to another
+
+echo "Enter the current full base URL (eg: http://mysite.dev), followed by [ENTER]:"
+
+read current_url
+
+echo "Enter the destination full base URL (eg: http://mysite.com), followed by [ENTER]:"
+
+read dest_url
+
+source .env
+
+php ./vendor/interconnectit/search-replace-db/srdb.cli.php -h DB_HOST -n DB_DATABASE -u DB_USERNAME -p DB_PASSWORD -s $current_url -r $dest_url
+
+echo "Done!"
