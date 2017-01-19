9551856b8953d65b97c4851e6d450b75fe70adf9
Matt Fox <matt.fox@theplanworks.com>
2017-01-19 14:02:36 -0500

    moving migrate script to Class-based

---
+++ code/scripts/thePLAN/Composer/scripts.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace thePLAN\Composer;
+
+use Composer\Script\Event;
+use Dotenv\Dotenv;
+
+class Scripts
+{
+    public static function migrate(Event $event) {
+
+        $composer = $event->getComposer();
+        $io = $event->getIO();
+
+        $vendorPath = $composer->getConfig()->get('vendor-dir');
+        $dotenv = new Dotenv(dirname($vendorPath));
+        $dotenv->load();
+
+        // Migrate DB from one domain to another
+        $current_url = $io->ask('Enter the current full base URL (eg: http://mysite.dev), followed by [ENTER]: ');
+        $dest_url = $io->ask('Enter the destination full base URL (eg: http://mysite.com), followed by [ENTER]: ');
+
+        exec('php ./vendor/interconnectit/search-replace-db/srdb.cli.php -h ' . getenv('DB_HOST') . ' -n ' . getenv('DB_DATABASE') . ' -u ' . getenv('DB_USERNAME') . ' -p ' . getenv('DB_PASSWORD') . ' -s ' . escapeshellarg($current_url) . ' -r ' . escapeshellarg($dest_url));
+
+        $io->write("Done!");
+    }
+}
