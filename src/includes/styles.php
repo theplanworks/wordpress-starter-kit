4fdfbb1445e089ae895f08212233b3c0f1d138dd
Matt Fox <matt.fox@theplanworks.com>
2021-04-07 20:47:25 -0400

    fixing path for stylesheet

--- src/includes/styles.php
+++ src/includes/styles.php
@@ -2,6 +2,6 @@
 
 function theme_styles()
 {
-    wp_enqueue_style('theme-style', mix('build/css/styles.css'));
+    wp_enqueue_style('theme-style', mix('style.css'));
 }
 add_action('wp_print_styles', 'theme_styles');
